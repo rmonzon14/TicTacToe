3d53746b7a882f8dbbebbf28e766bbae4f7d0476
Red Monzon <redkmonzon30@gmail.com>
2023-04-06 15:18:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const gameBoard = () => {
+const gameBoard = (() => {
     let gameBoardData = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-
-    const getGameboard = () => {
-        return gameBoardData;
-    }
 
     const setGameBoard = (i, data) => {
         gameBoardData[i] = data;
     }
 
-    return {getGameboard, setGameBoard}
+    const getGameBoard = () => {
+        return gameBoardData;
+    }
+
+    return {getGameBoard, setGameBoard}
+})();
+
+const Player = (sign) => {
+    let score = 0;
+
+    const getScore = () => score;
+
+    const getSign = () => sign;
+
+    const updateScore = () => score += 1;
+
+    return {getScore, getSign, updateScore}
 }
+
+const gameBoardController = (() => {
+    const squaresDivs = document.getElementsByClassName("squares");    
+    const squares = Array.from(squaresDivs);
+
+    squares.forEach((square, i) => {
+        square.addEventListener("click", ()  => {
+            square.textContent = gameBoard.getGameBoard()[i];
+        });
+    })
+
+})();
+
